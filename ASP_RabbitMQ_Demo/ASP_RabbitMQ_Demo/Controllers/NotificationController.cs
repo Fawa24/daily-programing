04667e45e4cc44f3efcca9f22677cfe5cdadedb1
Fawa24 <ofeglol@gmail.com>
2024-06-07 23:55:32 +0300

    Add ASP_RabbitMQ_Demo project + implement Producer

---
+++ ASP_RabbitMQ_Demo/ASP_RabbitMQ_Demo/Controllers/NotificationController.cs
@@ -0,0 +1,45 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace Producer.Controllers
+{
+	[ApiController]
+	public class NotificationController : ControllerBase
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		const string url = "";
+
+		public NotificationController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<string>> SendNotifications()
+		{
+			var list = Notification.GetNotifications();
+
+			// Invoking another API
+
+			HttpClient client = _httpClientFactory.CreateClient();
+
+			StringContent jsonContent = new StringContent(
+				content: JsonSerializer.Serialize(list),
+				encoding: Encoding.UTF8,
+				mediaType: "application/json");
+
+			HttpResponseMessage response = await client.PostAsync(url, jsonContent);
+
+			if (response.IsSuccessStatusCode)
+			{
+				return Ok(list);
+			}
+			else
+			{
+				return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+			}
+		}
+	}
+}
---
+++ ASP_RabbitMQ_Demo/Entities/Notification.cs
@@ -0,0 +1,26 @@
+﻿namespace Entities
+{
+	public class Notification
+	{
+		public string? Sender { get; set; }
+		public string? Message { get; set; }
+		public string? Receiver { get; set; }
+
+		public static List<Notification> GetNotifications()
+		{
+			var list = new List<Notification>()
+			{
+				new Notification { Sender = "Sender1", Message = "Alarm!", Receiver = "Receiver2" },
+				new Notification { Sender = "Sender1", Message = "Alarm!", Receiver = "Receiver3" },
+				new Notification { Sender = "Sender1", Message = "Alarm!", Receiver = "Receiver4" },
+				new Notification { Sender = "Sender1", Message = "Alarm!", Receiver = "Receiver5" },
+				new Notification { Sender = "Sender2", Message = "Alarm!", Receiver = "Receiver6" },
+				new Notification { Sender = "Sender2", Message = "Alarm!", Receiver = "Receiver7" },
+				new Notification { Sender = "Sender2", Message = "Alarm!", Receiver = "Receiver8" },
+				new Notification { Sender = "Sender2", Message = "Alarm!", Receiver = "Receiver9" }
+			};
+
+			return list;
+		}
+	}
+}
