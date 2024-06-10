5928fb19d528242d09c77bc821ed43ed0616174e
Fawa24 <ofeglol@gmail.com>
2024-06-10 08:52:50 +0300

    Upload changes

---
+++ Identify_demo/Identify_demo.Core/Domain/RepositoriesContracts/INotificationRepository.cs
@@ -0,0 +1,11 @@
+﻿using Identify_demo.Core.DTO;
+
+namespace Identify_demo.Core.Domain.RepositoriesContracts
+{
+	public interface INotificationRepository
+	{
+		public Task AddNotification(Notification notification);
+		public Task<List<Notification>> GetUserNotifications(string username);
+		public Notification ToNotification(AddNotificationRequest request);
+	}
+}
---
+++ Identify_demo/Identify_demo.Core/Services/NotificationRequestService.cs
@@ -0,0 +1,16 @@
+﻿using Identify_demo.Core.Domain.RepositoriesContracts;
+
+namespace Identify_demo.Core.Services
+{
+	public class NotificationRequestService
+	{
+		private readonly INotificationRepository _repository;
+
+		public NotificationRequestService(INotificationRepository repository)
+		{
+			_repository = repository;
+		}
+
+
+	}
+}
