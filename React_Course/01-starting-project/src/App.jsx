32a84e3182ee59e035e38df1e45fdf298df18e96
Fawa24 <ofeglol@gmail.com>
2024-06-12 15:51:26 +0300

    Put components in different files

--- React_Course/01-starting-project/src/App.jsx
+++ React_Course/01-starting-project/src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from './assets/react-core-concepts.png'
-import componentsImage from './assets/components.png'
 import { CORE_CONCEPTS } from './data.js';
-
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() 
-{
-  return(
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {reactDescription[genRandomInt(2)]} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title}/>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import { Header } from './components/header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
 function App() {
   return (
