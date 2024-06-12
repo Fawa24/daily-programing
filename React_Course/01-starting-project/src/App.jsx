da8c8ab7732bc7bc0d28081a31a91458e0a0073b
Fawa24 <ofeglol@gmail.com>
2024-06-12 21:00:44 +0300

    Outputting list data dynamically

--- React_Course/01-starting-project/src/App.jsx
+++ React_Course/01-starting-project/src/App.jsx
@@ -36,10 +36,7 @@ function App() {
 
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem}/>)}
           </ul>
         </section>
 
