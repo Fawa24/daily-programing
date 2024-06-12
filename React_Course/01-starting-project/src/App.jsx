3928723aab69794df3429bc67b927c94c85fbabf
Fawa24 <ofeglol@gmail.com>
2024-06-12 18:02:48 +0300

    Use the 'children' prop

--- React_Course/01-starting-project/src/App.jsx
+++ React_Course/01-starting-project/src/App.jsx
@@ -1,13 +1,16 @@
 import { CORE_CONCEPTS } from './data.js';
 import { Header } from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton/TabButton.jsx';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
+
         <section id="core-concepts">
+
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept {...CORE_CONCEPTS[0]} />
@@ -16,7 +19,17 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Component</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
+
       </main>
     </div>
   );
