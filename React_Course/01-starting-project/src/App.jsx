d17de34df2aa90dc168487717494ce4c5f6841ee
Fawa24 <ofeglol@gmail.com>
2024-06-12 10:38:35 +0300

    Add dynamic values output

--- React_Course/01-starting-project/src/App.jsx
+++ React_Course/01-starting-project/src/App.jsx
@@ -1,3 +1,9 @@
+const reactDescription = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() 
 {
   return(
@@ -5,12 +11,14 @@ function Header()
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {reactDescription[genRandomInt(2)]} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
   );
 }
+
+
 
 function App() {
   return (
