a76ed1c8212bbc5f4dd5ef5ab4c73b2a6bac15a5
Fawa24 <ofeglol@gmail.com>
2024-06-13 09:31:13 +0300

    Split components by feature and state

--- React_Course/01-starting-project/src/App.jsx
+++ React_Course/01-starting-project/src/App.jsx
@@ -1,56 +1,15 @@
-import { CORE_CONCEPTS } from './data.js';
 import { Header } from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton/TabButton.jsx';
-import { useState } from 'react';
-import { EXAMPLES } from './data-with-examples.js';
+import CoreConcepts from './components/CoreConcepts/CoreConcepts.jsx';
+import Examples from './components/Examples/Examples.jsx';
+
 
 function App() {
-  let [ selectedTopic, setSelectedTopic ] = useState();
-
-  function handleClick(selectedButton) {
-    setSelectedTopic(selectedButton)
-  }
-
-  let tabContent = <p>Please select a topic using buttons above</p>
-
-  if (selectedTopic) {
-    tabContent = 
-    <div id="tab-content">            
-      <h3>{EXAMPLES[selectedTopic].title}</h3>
-      <p>{EXAMPLES[selectedTopic].description}</p>
-      <pre>
-        <code>
-          {EXAMPLES[selectedTopic].code}
-        </code>
-      </pre>
-    </div>
-  }
-
   return (
     <>
       <Header></Header>
       <main>
-
-        <section id="core-concepts">
-
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem}/>)}
-          </ul>
-        </section>
-
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isClicked={selectedTopic === 'components'} onClick={() => handleClick("components")}>Components</TabButton>
-            <TabButton isClicked={selectedTopic === 'jsx'} onClick={() => handleClick("jsx")}>JSX</TabButton>
-            <TabButton isClicked={selectedTopic === 'props'} onClick={() => handleClick("props")}>Props</TabButton>
-            <TabButton isClicked={selectedTopic === 'state'} onClick={() => handleClick("state")}>State</TabButton>
-          </menu>
-          {tabContent}          
-        </section>
-
+        <CoreConcepts/>
+        <Examples/>
       </main>
     </>
   );
