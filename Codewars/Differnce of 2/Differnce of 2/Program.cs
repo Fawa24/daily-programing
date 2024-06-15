c646ab7e12b33a508b9ef09d181859c73c9c828c
Fawa24 <ofeglol@gmail.com>
2024-06-15 08:50:23 +0300

    Solve Difference of 2 Codewars task

---
+++ Codewars/Differnce of 2/Differnce of 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+	public static (int, int)[] TwosDifference(int[] array)
+	{
+		Array.Sort(array);
+		List<(int, int)> list = new List<(int, int)>();
+
+		for (int i = 0; i < array.Length; i++)
+		{
+			for (int j = i; j < array.Length; j++)
+			{
+				if (Math.Abs(array[i] - array[j]) == 2)
+				{
+					list.Add((array[i], array[j]));
+				}
+			}
+		}
+
+		(int, int)[] result = list.ToArray();
+
+		return result;
+	}
+}
+
+public static class Program
+{
+	public static void Main(string[] args)
+	{
+
+	}
+}
