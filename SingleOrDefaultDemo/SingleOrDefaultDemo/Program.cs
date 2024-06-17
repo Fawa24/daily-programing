ece54f4186b426a30a55641642c318a0c7155109
Fawa24 <ofeglol@gmail.com>
2024-06-17 12:49:53 +0300

    LINQ Single vs SingleOrDefault demo

---
+++ SingleOrDefaultDemo/SingleOrDefaultDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+	public static void Main()
+	{
+		List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+		try
+		{
+			int singleElement = numbers.SingleOrDefault(t => t == 6);
+			Console.WriteLine("Single Element: " + singleElement);
+		}
+		catch (InvalidOperationException ex)
+		{
+			Console.WriteLine("Error: " + ex.Message);
+		}
+
+		List<int> multipleNumbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+		try
+		{
+			int singleElement = multipleNumbers.Single();
+			Console.WriteLine("Single Element: " + singleElement);
+		}
+		catch (InvalidOperationException ex)
+		{
+			Console.WriteLine("Error: " + ex.Message);
+		}
+	}
+}
