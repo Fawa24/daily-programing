e18269db47c1a4c690e32925bcce0a2e39f70da5
Fawa24 <ofeglol@gmail.com>
2024-06-22 12:40:10 +0300

    Add Action filter to Index method of the PersonController

---
+++ Filters_Harsha/CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ActionFilters
+{
+	public class PersonsListActionFilter : IActionFilter
+	{
+		private readonly ILogger<PersonsListActionFilter> logger;
+
+		public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+		{
+			this.logger = logger;
+		}
+
+		public void OnActionExecuted(ActionExecutedContext context)
+		{
+			logger.LogInformation("PersonsListActionFilter.OnActionExecuted");
+		}
+
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			logger.LogInformation("PersonsListActionFilter.OnActionExecuting");
+		}
+	}
+}
