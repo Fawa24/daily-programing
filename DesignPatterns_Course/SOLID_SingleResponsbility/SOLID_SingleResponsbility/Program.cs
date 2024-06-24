311453587eab8a5c102bf75c71eb5e1f456035a8
Fawa24 <ofeglol@gmail.com>
2024-06-24 21:50:33 +0300

    Add starting code for a single responsibility principle demo

---
+++ DesignPatterns_Course/SOLID_SingleResponsbility/SOLID_SingleResponsbility/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DesignPatterns
+{
+	public class Journal
+	{
+		private readonly List<string> entries = new List<string>();
+
+		private static int count = 0;
+
+		public int AddEntry(string entry)
+		{
+			entries.Add($"{++count}: {entry}");
+			return count; // memento???
+		}
+
+		public void RemoveEntry(int index)
+		{
+			entries.RemoveAt(index);
+		}
+
+		public override string ToString()
+		{
+			return string.Join(Environment.NewLine, entries);
+		}
+	}
+
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			var journal = new Journal();
+
+			journal.AddEntry("I cried today");
+			journal.AddEntry("I ate a bug");
+
+			Console.WriteLine(journal.ToString());
+		}
+	}
+}
