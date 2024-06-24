742a415708b747310060aac2a2afe08042a7c177
Fawa24 <ofeglol@gmail.com>
2024-06-24 09:59:58 +0300

    Validate action arguments using OnActionExecuting

--- Filters_Harsha/CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
+++ Filters_Harsha/CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -1,24 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using ServiceContracts.DTO;
 
 namespace CRUDExample.Filters.ActionFilters
 {
 	public class PersonsListActionFilter : IActionFilter
 	{
-		private readonly ILogger<PersonsListActionFilter> logger;
+		private readonly ILogger<PersonsListActionFilter> _logger;
 
 		public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
 		{
-			this.logger = logger;
+			this._logger = logger;
 		}
 
 		public void OnActionExecuted(ActionExecutedContext context)
 		{
-			logger.LogInformation("PersonsListActionFilter.OnActionExecuted");
+			_logger.LogInformation("PersonsListActionFilter.OnActionExecuted");
 		}
 
 		public void OnActionExecuting(ActionExecutingContext context)
 		{
-			logger.LogInformation("PersonsListActionFilter.OnActionExecuting");
+			_logger.LogInformation("PersonsListActionFilter.OnActionExecuting");
+
+			if (context.ActionArguments.ContainsKey("searchBy"))
+			{
+				string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);
+
+				if (!string.IsNullOrEmpty(searchBy))
+				{
+					var searchByOptions = new List<string>()
+					{
+						nameof(PersonResponse.PersonName),
+						nameof(PersonResponse.Email),
+						nameof(PersonResponse.DateOfBirth),
+						nameof(PersonResponse.Gender),
+						nameof(PersonResponse.CountryID),
+						nameof(PersonResponse.Address),
+					};
+
+					if (searchByOptions.Any(temp => temp == searchBy) == false)
+					{
+						_logger.LogInformation("searchBy actual value {searchBy}", searchBy);
+						context.ActionArguments["searchBy"] = nameof(PersonResponse.PersonName);
+
+						_logger.LogInformation("searchBy updated value {searchBy}", context.ActionArguments["searchBy"]);
+					}
+				}
+			}
 		}
 	}
 }
