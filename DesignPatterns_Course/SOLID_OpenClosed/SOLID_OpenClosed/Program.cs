e66e6184668b141d98693d200e5a0cc291cbcbd2
Fawa24 <ofeglol@gmail.com>
2024-06-24 22:18:51 +0300

    Add starting code for a open-closed principle demo

---
+++ DesignPatterns_Course/SOLID_OpenClosed/SOLID_OpenClosed/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace DesignPatterns
+{
+	public enum Color
+	{
+		Red, Green, Blue
+	}
+
+	public enum Size
+	{
+		Small, Medium, Large, Huge
+	}
+
+	public class Product
+	{
+		public string Name;
+		public Color Color;
+		public Size Size;
+
+		public Product(string name, Color color, Size size)
+		{
+			Name = name;
+			Color = color;
+			Size = size;
+		}
+	}
+
+	public class ProductFilter
+	{
+		public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+		{
+			foreach (Product product in products)
+			{
+				if (product.Size == size)
+				{
+					yield return product;
+				}
+			}
+		}
+	}
+
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			var apple = new Product("Apple", Color.Green, Size.Small);
+			var tree = new Product("Tree", Color.Green, Size.Large);
+			var house = new Product("House", Color.Blue, Size.Huge);
+
+			Product[] products = { apple, tree, house };
+
+			var productFilter = new ProductFilter();
+		}
+	}
+}
