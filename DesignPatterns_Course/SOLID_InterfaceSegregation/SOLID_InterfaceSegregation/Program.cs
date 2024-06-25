5f21692dee937a0911601d562dad8967840603f2
Fawa24 <ofeglol@gmail.com>
2024-06-25 08:36:28 +0300

    Add starting code for a Interface Segregation principle demo

---
+++ DesignPatterns_Course/SOLID_InterfaceSegregation/SOLID_InterfaceSegregation/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace DesignPatterns
+{
+	public class Document
+	{
+
+	}
+
+	public interface IMAchine
+	{
+		void Print(Document document);
+		void Scan(Document document);
+		void Fax(Document document);
+	}
+
+	public class MultiFunctionPrinter : IMAchine
+	{
+		public void Fax(Document document)
+		{
+			// implementation
+		}
+
+		public void Print(Document document)
+		{
+			// implementation
+		}
+
+		public void Scan(Document document)
+		{
+			// implementation
+		}
+	}
+
+	public class OldFashionedPrinter : IMAchine
+	{
+		public void Fax(Document document)
+		{
+			// implementation
+		}
+
+		public void Print(Document document)
+		{
+			throw new NotImplementedException(); // no implementation
+		}
+
+		public void Scan(Document document)
+		{
+			throw new NotImplementedException(); // no implementation
+		}
+	}
+
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+
+		}
+	}
+}
