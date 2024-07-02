7ef3ff35bdf17fd2ae785d28fca460948aa98667
Fawa24 <ofeglol@gmail.com>
2024-07-02 09:33:49 +0300

    Delete all the React-Course resources

--- React_Course/01-starting-project/src/components/TabButton/TabButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function TabButton({children, onClick, isClicked}) {
-    return (
-        <li>
-            <button className={isClicked ? 'active' : undefined} onClick={onClick}>{children}</button>
-        </li>
-    );
-}
--- React_Course/task-apps/coding-exercise-14/src/Input.js
+++
@@ -1,5 +0,0 @@
-export default function Input({richText, ...props }) {
-    return (
-        <input type={richText ? "textarea" : "text"} {...props} />
-    )
- } 
