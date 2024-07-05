3966d47342e852a744bda39370c45b2ae80cff45
Fawa24 <ofeglol@gmail.com>
2024-07-05 10:27:25 +0300

    Create content that displays conditionaly

--- React_Course/#My_projects/simple-input-form-demo/src/App.js
+++ React_Course/#My_projects/simple-input-form-demo/src/App.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const[isDisplayed, setIsDisplayed] = useState(true)
+
+  const content = <div>
+    <h3>Hello, User!</h3>
+    <p>This is some random content</p>
+  </div>
+
+  function handleOnClick() {
+    setIsDisplayed(wasDisplayed => !wasDisplayed)
+  }
+
   return (
     <div>
-      
+      <button onClick={handleOnClick}>Click me</button>
+      {isDisplayed ? content : undefined}
     </div>
   );
 }
