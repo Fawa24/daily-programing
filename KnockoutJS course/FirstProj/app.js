acb7bde8f24b8c5ce27c0f54f163332f5c4b8f90
Fawa24 <ofeglol@gmail.com>
2024-07-19 16:42:28 +0300

    Use 'self' variable pattern and computed values

--- KnockoutJS course/FirstProj/app.js
+++ KnockoutJS course/FirstProj/app.js
@@ -1,7 +1,15 @@
-var viewModel = {
-  todo: ko.observable('Create responsive UI component using KnockoutJS'),
-  completed: 'No',
-  works: 'Yes'
+var viewModel = function() {
+  var self=this; 
+  self.todo = ko.observable('Create responsive UI component using KnockoutJS'),
+  self.completed = ko.observable('No'),
+  self.state = ko.computed({
+    read: function() { return self.todo() + ": " + self.completed === 'Yes' ? 'Completed' : 'NotCompleted' },
+    write: function(value) {
+      var arrName = value.split(' ')
+      self.todo(arrName[0])
+      self.completed(arrName[1])
+    }
+  })
 }
 
 ko.applyBindings(viewModel);
