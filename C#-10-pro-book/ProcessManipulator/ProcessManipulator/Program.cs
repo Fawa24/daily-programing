a13497c4c4fe72b52a7c0143d8e780ac23ccdc92
Fawa24 <ofeglol@gmail.com>
2024-08-27 14:03:50 +0300

    Wrote the program for listing all running processes

--- C#-10-pro-book/ProcessManipulator/ProcessManipulator/Program.cs
+++ C#-10-pro-book/ProcessManipulator/ProcessManipulator/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ProcessManipulatorDemo
+﻿using System.Diagnostics;
+
+namespace ProcessManipulatorDemo
 {
 	public static class Program
 	{
 		public static void Main(string[] args)
 		{
+			ListAllRunningProcesses();
+		}
 
+		/// <summary>
+		/// Writes all the curently running processes into the console
+		/// </summary>
+		static void ListAllRunningProcesses()
+		{
+			// First, get all the running processes ordered by PID
+
+			var runningProcesses = Process.GetProcesses();
+
+			foreach (var process in runningProcesses)
+			{
+				process.Display();
+			}
+		}
+	}
+
+	public static class ProcessExtension
+	{
+		public static void Display(this Process process)
+		{
+			Console.WriteLine($"PID: {process.Id}\t Name :{process.ProcessName} ");
 		}
 	}
 }
