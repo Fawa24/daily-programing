1237c3096cd2fd82f68be6edcb1b2b3d2efbafd5
Fawa24 <ofeglol@gmail.com>
2024-09-01 17:48:34 +0300

    Added props passing to CoreConcept component

--- React_Course/react-essentials-1-starting-project/src/App.jsx
+++ React_Course/react-essentials-1-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 const adjectivesArray = ['Fundamental', 'Crucial', 'Core'];
-import reactImage from './assets/react-core-concepts.png'
+import reactImage from './assets/react-core-concepts.png';
+import componentsImage from './assets/components.png'
 
 function getRandomAdjective() {
   let index = Math.floor(Math.random() * (adjectivesArray.length));
@@ -11,12 +12,30 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
+          <h2>Concepts</h2>
+          <ul>
+            <CoreConcept title='Components' description='The core UI building block' img={componentsImage}/>
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
   );
 }
 
+function CoreConcept({title, description, img}) {
+  return(
+    <li>
+      <img src={img} alt=''/>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
 
 function Header() {
 const description = getRandomAdjective();
