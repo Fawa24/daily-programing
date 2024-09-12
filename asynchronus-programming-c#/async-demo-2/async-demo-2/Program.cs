4d0ad2d92af38a9928f616e672fee020f65df5e0
Fawa24 <ofeglol@gmail.com>
2024-09-12 10:39:38 +0300

    Created demo project for async practice (boiling water)

---
+++ asynchronus-programming-c#/async-demo-2/async-demo-2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DemoApp
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			MakeTea();
+		}
+
+		public static string MakeTea()
+		{
+			var water = BoilWater();
+			Console.WriteLine("take the cups out");
+			Console.WriteLine("put tea in cups");
+			var tea = $"pour {water} in cups";
+			Console.WriteLine(tea);
+			return tea;
+		}
+
+		public static string BoilWater()
+		{
+			Console.WriteLine("Start the kettle");
+			Task.Delay(2000).GetAwaiter().GetResult();
+
+			Console.WriteLine("kettle finished boiling");
+
+			return "water";
+		}
+	}
+}
