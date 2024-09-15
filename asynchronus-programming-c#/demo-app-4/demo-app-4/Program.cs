1b435170e540736bb054fcf879e1756101362525
Fawa24 <ofeglol@gmail.com>
2024-09-15 11:56:46 +0300

    Added one more demo project

--- asynchronus-programming-c#/demo-app-4/demo-app-4/Program.cs
+++ asynchronus-programming-c#/demo-app-4/demo-app-4/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace demo_app_4
+﻿using System.Diagnostics;
+
+namespace demo_app_4
 {
-	internal class Program
+	public static class Program
 	{
-		static void Main(string[] args)
+		private readonly static string PathToFile = "books\\third-book.txt";
+
+		public static async Task Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			ActSync();
+			await ActAsync();
+		}
+
+		public static int CountWords()
+		{
+			char[] delimiters = new char[] { ' ', '\r', '\n', '.', ',', ';', ':', '!', '?' };
+			var fileReader = new StreamReader(PathToFile);
+			string text = fileReader.ReadToEnd();
+			string[] words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+			int count = words.Length;
+
+			return count;
+		}
+
+		public static Task<int> CountWordsTask()
+		{
+			return Task.Run(() =>
+			{
+				char[] delimiters = new char[] { ' ', '\r', '\n', '.', ',', ';', ':', '!', '?' };
+				var fileReader = new StreamReader(PathToFile);
+				string text = fileReader.ReadToEnd();
+				string[] words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+				int count = words.Length;
+
+				return count;
+			});
+		}
+
+		public static void ActSync()
+		{
+			var stopwatch = new Stopwatch();
+			stopwatch.Start();
+
+			var count = CountWords();
+			Task.Delay(5000).Wait();
+
+			Console.WriteLine($"Count: {count}\t Time: {stopwatch.ElapsedMilliseconds.ToString()}");
+		}
+
+		public static async Task ActAsync()
+		{
+			var stopwatch = new Stopwatch();
+			stopwatch.Start();
+
+			var task = CountWordsTask();
+			Task.Delay(5000).Wait();
+			var count = await task;
+
+			Console.WriteLine($"Count: {count}\t Time: {stopwatch.ElapsedMilliseconds.ToString()}");
 		}
 	}
 }
