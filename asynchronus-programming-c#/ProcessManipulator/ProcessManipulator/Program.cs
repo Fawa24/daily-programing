94ac1a9078f0f0da6084b6056853fac820def23d
Fawa24 <ofeglol@gmail.com>
2024-09-16 09:48:18 +0300

    Added ProcessManipulator project

---
+++ asynchronus-programming-c#/ProcessManipulator/ProcessManipulator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace ProcessManipulator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var runningProcesses =
+				from proc
+				in Process.GetProcesses()
+				orderby proc.Id
+				select proc;
+
+			foreach (var proc in runningProcesses)
+			{
+				Console.WriteLine($"PID: {proc.Id}\tName: {proc.ProcessName}");
+			}
+		}
+	}
+}
