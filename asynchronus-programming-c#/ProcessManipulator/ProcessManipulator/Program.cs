81d202126e1fb6f9aa60c5a6077fad4e9471bd3d
Fawa24 <ofeglol@gmail.com>
2024-09-16 10:27:55 +0300

    Implement getting process by PID and listing its threads

--- asynchronus-programming-c#/ProcessManipulator/ProcessManipulator/Program.cs
+++ asynchronus-programming-c#/ProcessManipulator/ProcessManipulator/Program.cs
@@ -7,6 +7,7 @@ namespace ProcessManipulator
 		static void Main(string[] args)
 		{
 			ListCurrentProcesses();
+			EnumThreadsForPID(1384);
 		}
 
 		public static void ListCurrentProcesses()
@@ -19,8 +20,35 @@ namespace ProcessManipulator
 
 			foreach (var proc in runningProcesses)
 			{
-				Console.WriteLine($"PID: {proc.Id}\tName: {proc.ProcessName}");
+				Console.WriteLine($"PID: {proc.Id}  \tName: {proc.ProcessName}");
 			}
+			Console.WriteLine("\n**************************************************\n");
+		}
+
+		public static void EnumThreadsForPID(int pID)
+		{
+			Process proc = null;
+			Console.WriteLine("Trying to get the process by PID...");
+			try
+			{
+				proc = Process.GetProcessById(pID);
+				Console.WriteLine("Process was found successfully\n");
+			}
+			catch (ArgumentException ex)
+			{
+				Console.WriteLine(ex.Message);
+				return;
+			}
+
+			Console.WriteLine($"Here are the threads used by {proc.ProcessName}\n");
+
+			foreach (ProcessThread th in proc.Threads)
+			{
+				string info = $"Thread ID: {th.Id}  \tStart time: {th.StartTime.ToShortTimeString()} \tPriority: {th.PriorityLevel}";
+				Console.WriteLine(info);
+			}
+
+			Console.WriteLine("\n**************************************************\n");
 		}
 	}
 }
