08ac29229617f9d124b1c005ee3fc4b11ca866c5
Fawa24 <ofeglol@gmail.com>
2024-09-19 12:31:18 +0300

    Added DisplayAppDomainStats method

--- asynchronus-programming-c#/DefaultAppDomainApp/DefaultAppDomainApp/Program.cs
+++ asynchronus-programming-c#/DefaultAppDomainApp/DefaultAppDomainApp/Program.cs
@@ -4,7 +4,21 @@
 	{
 		static void Main(string[] args)
 		{
+			DisplayAppDomainStats();
+		}
 
+		static void DisplayAppDomainStats()
+		{
+			var appDomain = AppDomain.CurrentDomain;
+
+			Console.WriteLine($"FriendlyName: {appDomain.FriendlyName}");
+			Console.WriteLine($"Id: {appDomain.Id}");
+			Console.WriteLine($"IsDefaultAppDomain: {appDomain.IsDefaultAppDomain()}");
+			Console.WriteLine($"BaseDirectory: {appDomain.BaseDirectory}");
+			Console.WriteLine($"ApplicationBase: {appDomain.SetupInformation.ApplicationBase}");
+			Console.WriteLine($"TargetFrameworkName: {appDomain.SetupInformation.TargetFrameworkName}");
+
+			Console.WriteLine("\n**************************************************\n");
 		}
 	}
 }
